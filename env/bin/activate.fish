1e1aa658846056f11a78a0dd3e721bc336ad674c
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-05-26 12:03:42 +0300

    new files

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alicia/Phase3/Projects/Articles/env"
+set -gx VIRTUAL_ENV "/home/alicia/Phase3/Projects/Articles/Articles/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- env/lib/db/connection.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-def get_connection():
-	com = sqlite3.connect('articles.db')
-	com.row_factory = sqlite3.Row # This enables column access by name
-	return com
-    
